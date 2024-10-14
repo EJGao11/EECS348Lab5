b43cf47424446a13fdb8aaf970bcb116d4121553
Ethan_Gao <e784g298@home.ku.edu>
2024-10-13 20:21:31 -0500

    Fixing Matrixs

--- Sales/salesReporter.c
+++ Sales/salesReporter.c
@@ -2,146 +2,46 @@
 // KU ID: 3121537
 // Lab Session: Thrusday
 // Lab: 5
-// Collaborators: Frank Gao (Dad)
+// Collaborators: None
 
-#include <stdio.h>
+
 #include <stdlib.h>
-#include <stdbool.h>
-typedef struct
+#define SIZE 5 
+
+void readMatrix(int matrix[SIZE][SIZE], int rows, int cols)
 {
-  unsigned int month;
-  float sales;
-} MonthlySales;
-
-static const uint32_t sMonths = 12;
-
-static const char sMonthName[12][32] = 
-{
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-};
-
-static MonthlySales sData[sMonths];
-
-bool MonthReport(FILE* file)
-{
-  printf("Monthly Sales Report for 2024 \n\n");
-  printf("%-20s%-20s\n","Month","Sales");
-  for(uint32_t i=0; i < sMonths; i++)
+  printf("\nEnter elements: \n");
+  for (int i = 0; i < rows; i++) 
   {
-      char lineData[256];
-      if(fgets(lineData, sizeof(lineData), file))
-      {
-        sData[i].sales = atof(lineData);
-        sData[i].month = i;
-        printf("%-20s%5.2f\n", sMonthName[sData[i].month], sData[i].sales);
-      }
-      else
-      {
-        printf("Not enough data");
-        return false;
-      }
-  }
-  return true;
-}
-
-void SalesSummary()
-{
-  int minSalesIndex = 0;
-  int maxSalesIndex = 0;
-  double sumSales = sData[0].sales;
-  for(uint32_t i=1; i < sMonths; i++)
-  {
-    if (sData[i].sales < sData[minSalesIndex].sales)
+    for (int j = 0; j < cols; j++) 
     {
-      minSalesIndex = i;
+      printf("Row: %d, Col: %d\n", i, j);
+      scanf("%d", &matrix[i][j]);
     }
-    else if(sData[i].sales > sData[maxSalesIndex].sales)
-    {
-      maxSalesIndex = i;
-    }
-    sumSales += sData[i].sales;
   }
-
-  printf("\nSales summary report: \n\n");
-  printf("Minimum sales: %5.2f (%s)\n", sData[minSalesIndex].sales,
-    sMonthName[minSalesIndex]);
-  printf("Maximum sales: %5.2f (%s)\n", sData[maxSalesIndex].sales,
-    sMonthName[maxSalesIndex]);
-  printf("Average sales: %5.2f \n", sumSales/sMonths);
-  return;
 }
-
-void SixMonthAverage()
-{
-  printf("\nSix-Month moving average report: \n\n");
-  const u_int32_t sixMonth = 6;
-  for(uint32_t i=0; i < sixMonth+1; i++)
-  {
-    double sixMonthSum = 0;
-    for(uint32_t j=i; j < i+sixMonth; j++)
-    {
-      sixMonthSum += sData[j].sales;
-    }
-    char title[64];
-    sprintf(title,"%s-%-s ",sMonthName[i],sMonthName[i+5]);
-    printf("%-20s%5.2f\n",title, (sixMonthSum/sixMonth));
-  }
-  return;
-}
-
-int SortReport_CompareSales(const void* a, const void* b)
-{
-  if(((MonthlySales*)a)->sales > ((MonthlySales*)b)->sales)
-  { 
-    return -1;
-  }
-  if (((MonthlySales*)a)->sales < ((MonthlySales*)b)->sales)
-  {
-    return 1;
-  }
-  return 0;
-}
-
-void SortReport()
-{
-  qsort(sData, sMonths, sizeof(MonthlySales), SortReport_CompareSales);
-  printf("\nSales report (highest to lowest): \n\n");
-  printf("%-20s%-20s\n","Month","Sales");
-  for(uint32_t i=0; i < sMonths; i++)
-  {
-    printf("%-20s%5.2f\n", sMonthName[sData[i].month], sData[i].sales);
-  }
-  return;
-}
-
 int main()
 {
-  FILE* file = fopen("salesData.txt", "r");
+  int m1[SIZE][SIZE];
+  int m2[SIZE][SIZE];
 
-  if(file == NULL)
+  int cols1, rows1, cols2, rows2;
+  printf("Enter rows and columns for the first matrix: ");
+  scanf("%d%d", &rows1, &cols1);
+  if(rows1 > SIZE || cols1 > SIZE)
   {
-    printf("Your dumbs");
-    return 0;
+    printf("The max rows and cols is 5");
+    return -1;
   }
+  readMatrix(m1, rows1, cols1);
 
-  if(MonthReport(file))
+  printf("Enter rows and columns for the second matrix: ");
+  scanf("%d%d", &rows2, &cols2);
+  if(rows2 > SIZE || cols2 > SIZE)
   {
-    SalesSummary();
-    SixMonthAverage();
-    SortReport();
+    printf("The max rows and cols is 5");
+    return -1;
   }
-  fclose(file);
-
+  readMatrix(m2, rows2, cols2);
   return 0;
 }
